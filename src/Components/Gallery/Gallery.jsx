fc645e6959a71247d6fdce9abe076602d86a9c24
AnnaSobchenko <annsbchnk@gmail.com>
2022-11-02 17:12:50 +0200

    gallery update

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -4,12 +4,41 @@ import Modal from "../Modal/Modal";
 import s from "./Gallery.module.scss";
 const uuid = require("uuid");
 
-
 const Gallery = ({ arts }) => {
+  const [num, setNum] = useState(6);
+  const [isBtnMore, setIsBtnMore] = useState(true);
+  const [isBtnLess, setIsBtnLess] = useState(false);
+  let renderArts = [];
+  for (let i = 0; i < num; i++) {
+    renderArts.push(arts[i]);
+  }
+
   const [modal, setModal] = useState({
     open: false,
     content: null,
   });
+
+  const changeNumPlus = () => {
+    let diff = arts.length - num;
+    if (diff > 6) {
+      setNum((prev) => prev + 6);
+      setIsBtnLess(true);
+    } else if (diff <= 6) {
+      setNum(arts.length);
+      setIsBtnMore(false);
+    }
+  };
+  const changeNumMinus = () => {
+    let diff = renderArts.length - 6;
+    if (diff > 6) {
+      setNum((prev) => prev - 6);
+      setIsBtnLess(true);
+    } else if (diff <= 6) {
+      setNum(6);
+      setIsBtnMore(true);
+      setIsBtnLess(false);
+    }
+  };
 
   const openModal = (artInfo) => {
     setModal({
@@ -35,7 +64,7 @@ const Gallery = ({ arts }) => {
       <div className="container" id="gallery">
         <h2 className="visually-hidden">Gallery</h2>
         <ul className={s.gallery__list}>
-          {arts.map((art) => {
+          {renderArts.map((art) => {
             return (
               <li
                 className={s.gallery__item}
@@ -58,6 +87,24 @@ const Gallery = ({ arts }) => {
             );
           })}
         </ul>
+        {isBtnLess && (
+          <button
+            type="button"
+            onClick={() => changeNumMinus()}
+            className={`btn btn-outline-success ${s.gallery__btn_less}`}
+          >
+            Load less &nbsp; &#8679;
+          </button>
+        )}
+        {isBtnMore && (
+          <button
+            type="button"
+            onClick={() => changeNumPlus()}
+            className={`btn btn-outline-success ${s.gallery__btn_more}`}
+          >
+            Load more &nbsp; &#8681;
+          </button>
+        )}
         {modal.open && (
           <Modal handleClose={closeModal} checker={true}>
             <CardArt artInfo={modal.artInfo} closeModal={closeModal} />
